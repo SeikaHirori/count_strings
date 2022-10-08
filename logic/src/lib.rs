0d6d2b67724bf15c819b51ae3e8a535a65ba40e6
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-10-07 18:32:27 -0700

    TDD time. Lets go

--- logic/src/lib.rs
+++ logic/src/lib.rs
@@ -11,4 +11,24 @@ mod tests {
         let result = add(2, 2);
         assert_eq!(result, 4);
     }
+
+    #[test]
+    fn another_test() {
+        let result = add(3, 3);
+        assert_eq!(result, 6);
+    }
+
+    #[test]
+    fn string_splits() {
+        let expectations:[String; 2] = [String::from("hello"),
+            String::from("world"),
+        ];
+
+        let results = [String::from("fail"),
+            String::from("sadge"),
+        ];
+
+        assert_eq!(results, expectations);
+    }
+
 }
