fe11964f88471a862c27304879bee86fde0eb251
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-10-08 13:19:06 -0700

    Passed test for counting words

--- logic/src/lib.rs
+++ logic/src/lib.rs
@@ -1,19 +1,21 @@
 pub fn split_strings(user_string:String) -> Vec<String> {
     /* _Objective_
-    
         - Breakdown what is happening from RFER 03
-
     */
 
     let splited = user_string.split_whitespace();
     
     let mapped = splited.map(str::to_string);
     let output = mapped.collect();
+    
     output
-
 }
 
+pub fn count_words(word_list:Vec<String>) -> usize {
+    let amount = word_list.len();
 
+    amount
+}
 
 #[cfg(test)]
 mod tests {
@@ -37,6 +39,15 @@ mod tests {
         ); // RFER 02
     }
 
+    #[test]
+    fn test_counting_words() {
+        let user_string = String::from("hello world");
+        let word_list = split_strings(user_string);
+        
+        let results = count_words(word_list);
 
+        assert_eq!(results, 2)
+
+    }
 
 }
